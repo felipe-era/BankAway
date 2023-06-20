5a48875a07675ac0e6aff9b57fd642353c957bc6
feveloper <felip@FEVOLEPER>
2023-06-20 20:38:45 -0300

    InternalTests

--- BankAway_Modelos/BankAway.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
+++ BankAway_Modelos/BankAway.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
@@ -1,13 +1,19 @@
 ﻿using BankAway.Modelos.ADM.SistemaInterno;
+using BankAway_Modelos.BankAway.Modelos.ADM.Utilitario;
 
 namespace BankAway.Modelos.ADM.Utilitario
 {
     public class ParceiroComercial : IAutenticavel
     {
         public string Senha { get; set; }
+        public AutenticacaoUtil autenticador{ get; set; }
+
         public bool Autenticar(string senha)
         {
-            return Senha == senha;
+            return this.autenticador.ValidarSenha(this.Senha, senha);
         }
+
+
+
     }
 }
---
+++ BankAway_Modelos/BankAway.Modelos/ADM/Utilitario/AutenticacaoUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAway_Modelos.BankAway.Modelos.ADM.Utilitario
+{
+    internal class AutenticacaoUtil
+    {
+        public bool ValidarSenha(string senhaverdadeira , string senhatentativa)
+        {
+            return senhaverdadeira.Equals(senhatentativa);
+        }
+
+    }
+}
